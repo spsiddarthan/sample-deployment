6e02206d6b82607957919784a1d6e92ac1fdf6d2
Siddarthan S P <siddarthan@outlook.com>
2017-01-10 17:06:45 +0530

    Added pm2

--- app.js
+++ app.js
@@ -3,5 +3,5 @@ var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
   response.end("Hello there, this world is awesome!\n");
 });
-server.listen(80);
-console.log("Server running at http://127.0.0.1:80/");
+server.listen(9083);
+console.log("Server running at http://127.0.0.1:9083/");
