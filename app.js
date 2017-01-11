9d954ed76c00f3e0cf75ab4d923257b93c68f5ad
Siddarthan S P <siddarthan@outlook.com>
2017-01-11 12:24:51 +0530

    Buddy deployment

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello human, this is my second automatic deployment!\n");
+  response.end("Hello, deploying via buddy!\n");
 });
 server.listen(9083);
 console.log("Server running at http://127.0.0.1:9083/");
