bbbcc64964d7a1dc24059a1ec2dfa49076d59c1f
Siddarthan S P <siddarthan@outlook.com>
2017-01-11 16:13:17 +0530

    deploy

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello, deploying via buddy standalone!\n");
+  response.end("Hello, deploying via buddy standalone for the last time today!\n");
 });
 server.listen(9083);
 console.log("Server running at http://127.0.0.1:9083/");
